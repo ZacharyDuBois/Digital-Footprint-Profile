24016eb211a9ab06295fb492d7f0bb3c2199f895
Zachary DuBois <zacharydubois@icloud.com>
2015-12-09 11:37:22 -0500

    Add more templates.

---
+++ dfp/view/session/director.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * File: director.php
+ * User: zacharydubois
+ * Date: 2015-12-08
+ * Time: 21:55
+ * Project: Digital-Footprint-Profile
+ */
+
+$uri = explode('/', filter_input(INPUT_SERVER, 'REQUEST_URI'));
+
