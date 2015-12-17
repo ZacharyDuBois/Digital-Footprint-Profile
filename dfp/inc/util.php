afb072bda408fdc66ede475cd66341db18c970f0
Zachary DuBois <zacharydubois@icloud.com>
2015-12-16 19:17:20 -0500

    Add a bit more entropy.

--- dfp/inc/util.php
+++ dfp/inc/util.php
@@ -13,13 +13,14 @@ namespace DFP;
 class util {
     /**
      * Generates a session token
+     *
      * @return string
      */
     public static function genSessionToken() {
         $key = null;
 
         while (file_exists(SESSIONPATH . '/' . $key . '.json') || $key === null) {
-            $key = sha1(microtime(true) . '-' . rand(0, PHP_INT_MAX));
+            $key = sha1(microtime(true) . '-' . rand(0, PHP_INT_MAX) . filter_input(INPUT_SERVER, 'HTTP_USER_AGENT'));
         }
 
         return $key;
