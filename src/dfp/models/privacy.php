78a54c04bc11fd44daaf9696d1ac90e6c135fe13
Zachary DuBois <zacharydubois@icloud.com>
2016-01-05 12:26:55 -0500

    Privacy.

--- src/dfp/models/privacy.php
+++ src/dfp/models/privacy.php
@@ -6,3 +6,23 @@
  * Time: 20:48
  * Project: Digital-Footprint-Profile
  */
+
+namespace dfp;
+
+// Create objects.
+$view = new View();
+$nav = new Nav();
+
+// Create nav.
+$nav->setActive('privacy');
+
+// Tell view the nav array.
+$view->navArray($nav->navArray());
+
+// Tell view what template and content.
+$view->tpl('privacy');
+$view->content(array(
+    'title'       => 'Privacy | Digital Footprint Profile',
+));
+
+echo $view->render();
