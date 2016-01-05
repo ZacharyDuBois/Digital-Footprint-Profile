04d4aec17bba499e469e31692795f69df96a9fd7
Zachary DuBois <zacharydubois@icloud.com>
2016-01-05 09:55:20 -0500

    Create index model.

--- src/dfp/models/index.php
+++ src/dfp/models/index.php
@@ -6,3 +6,25 @@
  * Time: 20:47
  * Project: Digital-Footprint-Profile
  */
+
+namespace dfp;
+
+// Create objects.
+$view = new View();
+$nav = new Nav();
+$config = new Config();
+
+// Create nav.
+$nav->setActive('index');
+
+// Tell view the nav array.
+$view->navArray($nav->navArray());
+
+// Tell view what template and content.
+$view->tpl('index');
+$view->content(array(
+    'title'       => 'Start',
+    'sessionLink' => Utility::buildFullLink($config, true, 'session')
+));
+
+echo $view->render();
