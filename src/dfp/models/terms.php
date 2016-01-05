fcefa7d502aba778607ff739a3e7293c03172280
Zachary DuBois <zacharydubois@icloud.com>
2016-01-05 12:33:27 -0500

    Add terms.

--- src/dfp/models/terms.php
+++ src/dfp/models/terms.php
@@ -6,3 +6,23 @@
  * Time: 20:47
  * Project: Digital-Footprint-Profile
  */
+
+namespace dfp;
+
+// Create objects.
+$view = new View();
+$nav = new Nav();
+
+// Create nav.
+$nav->setActive('terms');
+
+// Tell view the nav array.
+$view->navArray($nav->navArray());
+
+// Tell view what template and content.
+$view->tpl('terms');
+$view->content(array(
+    'title'       => 'Terms | Digital Footprint Profile',
+));
+
+echo $view->render();
