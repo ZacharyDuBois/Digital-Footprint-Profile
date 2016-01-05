e08c45dbd7c6a5e3891662e876ceefcfb1125741
Zachary DuBois <zacharydubois@icloud.com>
2016-01-04 21:08:51 -0500

    Hopefully finish run.

--- src/dfp/controller/Run.php
+++ src/dfp/controller/Run.php
@@ -10,13 +10,47 @@
 namespace dfp;
 
 
+/**
+ * Class Run
+ *
+ * Runs app.
+ *
+ * @package dfp
+ */
 class Run {
-    public function __construct() {
-        $route = new Route();
-
-    }
-
+    /**
+     * Runs App
+     *
+     * Gets route, builds page, and echos rendered content.
+     *
+     * @return true
+     * @throws Exception
+     */
     public function letsGo() {
+        $route = new Route();
+        //$config = new Config();
+        $request = $route->guide();
+        $view = new View();
 
+
+        switch ($request) {
+            case 'redirect':
+                // Make sure redirects pass through with no returned content.
+                return true;
+            case false:
+                header("HTTP/1.1 404 Not Found");
+                $view->tpl('404');
+                break;
+            default:
+                $model = APP . 'models' . DS . basename($request) . '.php';
+                if (file_exists($model)) {
+                    require $model;
+                    break;
+                }
+        }
+
+        echo $view->render();
+
+        return true;
     }
 }
