5ae6e28d8c3f5a70bcb1d020ddf8f3990e522a51
Zachary DuBois <zacharydubois@icloud.com>
2016-01-04 23:20:06 -0500

    Fix loads.

--- src/dfp/load.php
+++ src/dfp/load.php
@@ -11,7 +11,7 @@ require COMPOSER;
 require APP . 'inc' . DS . 'Exception.php';
 require APP . 'inc' . DS . 'Validate.php';
 require APP . 'inc' . DS . 'Utility.php';
-require APP . 'functions' . DS . 'functions.php';
 require APP . 'controller' . DS . 'View.php';
 require APP . 'controller' . DS . 'DataStore.php';
 require APP . 'controller' . DS . 'Route.php';
+require APP . 'controller' . DS . 'Config.php';
