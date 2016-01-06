95e5e5a644217b7809045dca4d323e89529b5ea3
Zachary DuBois <zacharydubois@icloud.com>
2016-01-05 23:27:52 -0500

    Create Twitter callback.

--- src/dfp/models/twitter.php
+++ src/dfp/models/twitter.php
@@ -6,3 +6,15 @@
  * Time: 20:49
  * Project: Digital-Footprint-Profile
  */
+
+namespace dfp;
+
+$denied = filter_input(INPUT_REQUEST, 'denied');
+if (!isset($denied)) {
+// Create twitter.
+    $twitter = new Twitter();
+    $twitter->getPosts();
+}
+
+$config = new Config();
+header('Location: ' . Utility::buildFullLink($config, false, 'session'));
