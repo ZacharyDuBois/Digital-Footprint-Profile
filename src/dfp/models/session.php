93d57ca8fe643ae3dc9e634c9e83d5a0a8fe9b93
Zachary DuBois <zacharydubois@icloud.com>
2016-01-05 23:27:43 -0500

    Create session.

--- src/dfp/models/session.php
+++ src/dfp/models/session.php
@@ -6,3 +6,45 @@
  * Time: 20:48
  * Project: Digital-Footprint-Profile
  */
+
+namespace dfp;
+
+// Create session instance.
+$session = new Session();
+
+// Create config
+//$config = new Config();
+
+// Create Nav
+$nav = new Nav();
+$nav->setActive('session');
+
+// Create view.
+$view = new View();
+$view->navArray($nav->navArray());
+$view->tpl('session');
+
+// Twitter
+if ($session->getTMP('twitter_name') === false) {
+    $twitter = new Twitter();
+    $twitterButton = array(
+        'url'     => $twitter->authorizeURL(),
+        'text'    => 'Login with Twitter',
+        'classes' => ''
+    );
+} else {
+    $twitterButton = array(
+        'url'     => '#',
+        'text'    => 'Twitter: @' . $session->getTMP('twitter_name'),
+        'classes' => 'disabled'
+    );
+}
+
+// Render and return
+$view->content(array(
+    'title'        => 'Start | Digital Footprint Profile',
+    'loginButtons' => array(
+        $twitterButton,
+        // Others
+    )
+));
