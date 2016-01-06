8d4adab57db2b73f7d3518a5e5bd76d131485f6a
Zachary DuBois <zacharydubois@icloud.com>
2016-01-06 03:30:12 -0500

    Add simple listing.

--- src/dfp/models/list.php
+++ src/dfp/models/list.php
@@ -6,3 +6,45 @@
  * Time: 20:49
  * Project: Digital-Footprint-Profile
  */
+
+namespace dfp;
+
+// Config
+$config = new Config();
+
+// Session
+$session = new Session();
+
+if ($session->get('twitter') === false) {
+    header('Location: ' . Utility::buildFullLink($config, false, 'session'));
+}
+
+// View
+$nav = new Nav();
+$nav->setActive('list');
+$view = new View();
+$view->tpl('list');
+$view->navArray($nav->navArray());
+
+
+// Parse
+$parse = new Parse();
+
+// List array
+$list = array();
+
+foreach ($session->get('twitter') as $post) {
+    $parse->parse($post['content']);
+
+    $list[] = array(
+        'url'     => $post['url'],
+        'score'   => $parse->score(),
+        'tags'    => $parse->tags(),
+        'content' => $post['content']
+    );
+}
+
+$view->content(array(
+    'title' => 'Your Posts | Digital Footprint Profile',
+    'posts' => $list
+));
