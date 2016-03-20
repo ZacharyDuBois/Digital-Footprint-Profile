7460ba3981978085c1b742ea0100d0d754815e38
Zachary DuBois <me@zacharydubois.me>
2016-03-20 16:03:50 -0400

    Add a model for the callback.

--- src/dfp/models/facebook.php
+++ src/dfp/models/facebook.php
@@ -6,3 +6,18 @@
  * Time: 20:49
  * Project: Digital-Footprint-Profile
  */
+
+namespace dfp;
+
+$denied = filter_input(INPUT_GET, 'denied');
+
+$config = new Config();
+
+if (!isset($denied) || array_key_exists('oauth_verifier', $_GET)) {
+// Create twitter.
+    $twitter = new Twitter($config, new Session($config));
+    $twitter->accessToken();
+    $twitter->getPosts();
+}
+
+header('Location: ' . Utility::buildFullLink($config, false, 'session'));
